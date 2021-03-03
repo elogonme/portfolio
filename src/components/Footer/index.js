f226383a9dc7683d37d94315b850f7c0f023cfa6
Eldar <elogon@gmail.com>
2021-03-03 17:34:30 -0500

    Create footer component

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,29 @@
 import React from "react";
+import logo from '../../images/react-logo.svg';
 import "./style.css";
 
 function Footer() {
   return (
-    <footer className="footer">
-      <span>Pupster 2017</span>
+    <footer className="bg-secondary fixed-bottom">
+      <div className="text-light text-sm-center py-2 px-2 footer-text">
+        <img src={logo} className="react-logo float-left" alt="logo" />
+        <span className="float-left">Built with React</span>
+        © 2020 Copyright: Eldar Humbatov
+        <div className="float-right mr-4">
+          <a href="tel:+1-416-823-4666" target="_blank" rel="noreferrer" title="E-mail" className="text-white">
+            <i className="fas fa-phone-square px-2 zoom"></i>
+          </a>
+          <a href="mailto:eldarha@gmail.com" target="_blank" rel="noreferrer" title="E-mail" className="text-white">
+            <i className="fas fa-at px-2 zoom"></i>
+          </a>
+          <a href="https://www.linkedin.com/in/eldarhumbatov/" target="_blank" rel="noreferrer"  title="LinkedIn" className="text-white">
+            <i className="fab fa-linkedin-in px-2 zoom"></i>
+          </a>          
+          <a href="https://github.com/elogonme" target="_blank" rel="noreferrer"  title="GitHub" className="text-white">
+            <i className="fab fa-github px-2 zoom"></i>
+          </a>
+        </div>
+      </div>
     </footer>
   );
 }
