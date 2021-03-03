7aea07630bcccd09ff55e42c63497b899f2e17ba
Eldar <elogon@gmail.com>
2021-03-03 12:08:51 -0500

    setup react scaffolding boilerplate files

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Contact() {
+
+  return (
+    <div>
+      <input className="input-group" type="text" placeholder="Contact me"/>
+    </div>
+  );
+}
+
+export default Contact;
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,18 @@
+import React from "react";
+// import API from "../utils/API";
+// import Card from "../components/Card";
+// import Alert from "../components/Alert";
+
+function Portfolio() {
+
+    return (
+      <div>
+        <h1 className="text-center">My Portfolio</h1>
+        <h1 className="text-center">
+          React is the best!
+        </h1>
+      </div>
+    );
+}
+
+export default Portfolio;
